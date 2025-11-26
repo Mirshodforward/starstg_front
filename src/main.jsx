dd03e81f0de43d032e75d0fd97398c0195554443
Mirshod <forvardmf@gmail.com>
2025-11-26 15:05:54 +0500

    Yangi funksiya: mashina rangini tahrirlash

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './App.jsx'
+
+// Telegram Mini App SDK
+import WebApp from '@twa-dev/sdk'
+
+// Telegram WebApp ishga tayyor bo'lganda
+WebApp.ready()
+
+// Fonni va header rangini oq qilish
+WebApp.setBackgroundColor("#ffffff")
+WebApp.setHeaderColor("#ffffff")
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
